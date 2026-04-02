3fc1663fb713320e9bf9f109859cc702392681fb
Yonghyun <d0338b@gmail.com>
2026-04-02 15:15:35 +0900

    finish for sql file 02

--- test.py
+++ test.py
@@ -61,47 +61,34 @@ conn.commit()
 cursor.execute(
 """
 --sql
-WITH customer_summary AS(
-    SELECT c.customer_name, c.customer_grade, COUNT(s.order_id) AS total_order_count,
+WITH aging_summary AS(
+    SELECT c.customer_name, c.customer_grade, c.customer_id,
     COALESCE(SUM(s.amount),0) AS total_amount,
-
     SUM(CASE
-        WHEN julianday('2026-04-01') - julianday(s.order_date) <= 30 THEN s.amount
+        WHEN julianday('2026-04-02') - julianday(s.order_date) >= 61 THEN s.amount
         ELSE 0
-    END) AS current_0_30_amount,
-
-    SUM(CASE
-        WHEN julianday('2026-04-01') - julianday(s.order_date) >= 31 AND 
-        julianday('2026-04-01') - julianday(s.order_date) <= 60 THEN s.amount
-        ELSE 0
-    END) AS over_31_60_amount,
-
-    SUM(CASE
-        WHEN julianday('2026-04-01') - julianday(s.order_date) >= 61 THEN s.amount
-        ELSE 0
-    END) AS over_61_plus_amount
-
+    END) AS total_over_61_plus_amount
     FROM customers AS c
     LEFT JOIN sales AS s
     ON c.customer_id = s.customer_id
-    GROUP BY c.customer_name, c.customer_grade
-)
-
-SELECT customer_name, customer_grade, total_order_count, total_amount,
-current_0_30_amount, over_31_60_amount, over_61_plus_amount,
-
+    GROUP BY c.customer_name, c.customer_id, c.customer_grade
+),
+flag_summary AS(
+    SELECT customer_grade, total_amount, total_over_61_plus_amount,
     CASE
-        WHEN over_61_plus_amount > 0 THEN 'Risk'
+        WHEN total_over_61_plus_amount > 0 THEN 'Risk'
         ELSE 'Normal'
-    END AS risk_flag,
-
-    CASE
-        WHEN total_amount = 0 THEN 0
-        ELSE CAST(over_61_plus_amount AS REAL) / total_amount
-    END AS risk_ratio
-
-FROM customer_summary
-ORDER BY risk_ratio DESC, over_61_plus_amount DESC, customer_name ASC
+    END AS risk_flag
+    FROM aging_summary
+)
+SELECT customer_grade, COUNT(*) AS customer_count, SUM(total_amount) AS grade_total_amount,
+SUM(total_over_61_plus_amount) AS grade_total_over_61_plus_amount,
+SUM(CASE
+    WHEN risk_flag = 'Risk' THEN 1
+    ELSE 0
+END) AS risky_customer_count
+FROM flag_summary
+GROUP BY customer_grade
 ;
 """)
 rows = cursor.fetchall()
