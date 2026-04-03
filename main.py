5c4460ddcefeb4d5d9b0dce0c169d6b8d3e9735e
Yonghyun <d0338b@gmail.com>
2026-04-03 21:12:19 +0900

    plt file update for sql study

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sqlite3 
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 os.makedirs("output", exist_ok=True)
 
@@ -14,6 +15,22 @@ def save_sql_to_csv(sql_path, csv_path):
     print(f"Saved: {csv_path}")
     conn.close()
 
-save_sql_to_csv("sql/01_customer_aging_summary.sql", "output/customer_aging_summary.csv")
-save_sql_to_csv("sql/02_grade_risk_summary.sql", "output/grade_risk_summary.csv")
-save_sql_to_csv("sql/03_high_risk_customers.sql", "output/high_risk_customers.csv")
+def save_high_risk_chart(csv_path, image_path):
+    df = pd.read_csv(csv_path)
+
+    plt.figure(figsize=(8,5))
+    plt.bar(df["customer_name"], df["over_61_plus_amount"])
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig(image_path)
+    print(f"Saved: {image_path}")
+    plt.close()
+
+def main():
+    save_sql_to_csv("sql/01_customer_aging_summary.sql", "output/customer_aging_summary.csv")
+    save_sql_to_csv("sql/02_grade_risk_summary.sql", "output/grade_risk_summary.csv")
+    save_sql_to_csv("sql/03_high_risk_customers.sql", "output/high_risk_customers.csv")
+    save_high_risk_chart("output/high_risk_customers.csv","output/high_risk_customers_chart.png")
+    
+if __name__ == "__main__":
+    main()
